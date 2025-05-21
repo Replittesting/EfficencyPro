e43e826860e8cc333938145d34a6401b65b95ab7
Replittesting <vjcs2020@outlook.com>
2025-05-21 17:26:20 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,5 @@
-//import { createRoot } from "react-dom/client";
-//import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 //import "./index.css";
 
-//createRoot(document.getElementById("root")!).render(<App />);
-
-
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<h1 style={{ textAlign: "center", marginTop: "2rem" }}>Hello GitHub Pages 👋</h1>);
+createRoot(document.getElementById("root")!).render(<App />);
